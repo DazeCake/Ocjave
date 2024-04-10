e1ecb35e55b4c4a660812256221c7b6ada1a265d
DazeCake <dazecake@gmail.com>
2024-04-10 22:51:38 +0800

    feat: Init commit

---
+++ src/main/java/com/dazecake/ocjave/OcjaveApplication.java
@@ -0,0 +1,13 @@
+package com.dazecake.ocjave;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OcjaveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OcjaveApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dazecake/ocjave/OcjaveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dazecake.ocjave;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OcjaveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
